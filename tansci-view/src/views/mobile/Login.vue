69429fc71bfac1fe6c2828113e5ba59fa793e5df
tanyp <typ1805@qq.com>
2022-09-02 11:53:53 +0800

    更新

--- tansci-view/src/views/mobile/Login.vue
+++ tansci-view/src/views/mobile/Login.vue
@@ -1,34 +1,40 @@
 <template>
-    <div class="login-container">
-        <div class="login-header">
-            <div>账号登录</div>
-        </div>
+    <div class="login-container" :style="defaultStyle">
         <div class="login-main">
-            <el-card :shadow="shadow" :body-style="{padding: '3rem'}">
+            <el-card :shadow="shadow" :body-style="{padding: '2rem 3rem'}">
+                <div class="main-title">登录</div>
                 <el-form :model="userForm" :rules="rules" label-width="auto" ref="userFormRef">
-                    <el-form-item prop="username" label="账号" :rules="[{required: true,message: '请输入账号',trigger: 'blur'}]">
+                    <el-form-item prop="username" :rules="[{required: true,message: '请输入账号',trigger: 'blur'}]">
+                        <template #label>
+                            <el-icon :size="26" color="var(--theme)" style="padding-top: 0.2rem;"><User /></el-icon>
+                        </template>
                         <el-input v-model="userForm.username" placeholder="请输入账号" style="width: 100%;"></el-input>
                     </el-form-item>
-                    <el-form-item prop="password" label="密码" :rules="[{required: true,message: '请输入密码',trigger: 'blur'}]">
-                        <el-input v-model="userForm.password" type="password" placeholder="请输入密码" style="width: 100%;"></el-input>
+                    <el-divider />
+                    <el-form-item prop="password" :rules="[{required: true,message: '请输入密码',trigger: 'blur'}]">
+                        <template #label>
+                            <el-icon :size="26" color="var(--theme)" style="padding-top: 0.2rem;"><Lock /></el-icon>
+                        </template>
+                        <el-input v-model="userForm.password" type="password" placeholder="请输入密码" show-password style="width: 100%;"></el-input>
                     </el-form-item>
-                    <div style="padding-bottom: 2rem">
-                        <el-checkbox v-model="userForm.remember" label="记住密码"/>
-                    </div>
+                    <el-divider />
                     <div style="display: flex;justify-content: space-between;padding-bottom: 2rem">
-                        <el-button type="primary" link>忘记密码？</el-button>
                         <el-button @click="onRegister" type="primary" link>注册账号</el-button>
+                        <el-button type="primary" link>忘记密码？</el-button>
                     </div>
                     <div style="padding-bottom: 2rem">
                         <el-button @click="onSubmit" :loading="loading" type="primary" size="large" round style="width: 100%;">登录</el-button>
                     </div>
+                    <el-form-item prop="remember" :rules="[{required: true,message: '请勾选使用协议',trigger: 'blur'}]" class="item-remember">
+                        <el-checkbox v-model="userForm.remember" label="同意平台的《使用协议》"/>
+                    </el-form-item>
                 </el-form>
             </el-card>
         </div>
     </div>
 </template>
 <script setup>
-    import {reactive, ref, unref, toRefs} from 'vue'
+    import {onBeforeMount, reactive, ref, unref, toRefs} from 'vue'
     import {useRouter} from 'vue-router'
     import {useUserStore,useTokenStore} from '@/store/settings'
     import {login} from '@/api/admin/user'
@@ -40,30 +46,32 @@
     const state = reactive({
         shadow: 'always',
         loading: false,
+        defaultStyle: null,
         userForm: {
             username: '',
             password: '',
-            remember: '',
+            remember: null,
+            type: 2,
         },
     })
 
     const {
-        shadow,loading,userForm
+        shadow,loading,defaultStyle,userForm
     } = toRefs(state)
+
+    onBeforeMount(() => {
+        state.defaultStyle = {
+            height: (document.body.clientHeight || document.documentElement.clientHeight) + "px",
+        };
+    })
 
     const onSubmit = async () =>{
         const form = unref(userFormRef)
         if (!form) return;
         await form.validate()
 
-        let param = {
-			username: state.userForm.username,
-			password: state.userForm.password,
-            type: 2
-		}
-
 		state.loading = true;
-		login(param).then(res=>{
+		login(state.userForm).then(res=>{
 			if(res){
 				// 存储用户信息和token
 				userStore.setUser(res.result);
@@ -83,17 +91,37 @@
 </script>
 <style lang="scss">
     .login-container{
-        height: 100%;
-        .login-header{
-            text-align: center;
-            color: #fff;
-            font-size: 20px;
-            padding: 4rem 0;
-            background: var(--theme);
-        }
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-image: linear-gradient(to bottom, #f3e7e9 0%, #e3eeff 99%, #e3eeff 100%);
         .login-main{
+            padding: 0 3rem;
+            .main-title{
+                font-size: 2rem;
+                padding-bottom: 2rem;
+                font-weight: 700;
+            }
+            .item-remember{
+                .el-form-item__label-wrap{
+                    display: none;
+                }
+                .el-form-item__content{
+                    padding-left: 1rem;
+                }
+            }
+            .el-divider--horizontal{
+                margin: 0 0 3rem 0;
+                border-top: 2px solid #A8ABB2;
+            }
+            .el-form-item,.el-form-item--default{
+                margin-bottom: 0.4rem;
+            }
+            .el-form-item__error{
+                padding-top: 15px;
+            }
             .el-card{
-                margin: -2rem 2rem 0 2rem;
+                border-radius: 2rem;
             }
             .el-input__wrapper{
                 border: none !important;
--- tansci-view/src/views/mobile/Register.vue
+++ tansci-view/src/views/mobile/Register.vue
@@ -1,37 +1,40 @@
 <template>
-    <div class="register-container">
-        <div class="register-header">
-            <div>注册账号</div>
-        </div>
+    <div class="register-container" :style="defaultStyle">
         <div class="register-main">
-            <el-card :shadow="shadow" :body-style="{padding: '3rem'}">
+            <el-card :shadow="shadow" :body-style="{padding: '1.2rem 3rem'}">
+                <div class="main-title">注册</div>
                 <el-form :model="userForm" :rules="rules" label-position="right" label-width="auto" ref="userFormRef">
                     <el-form-item prop="username" label="账号" :rules="[{required: true,message: '请输入账号',trigger: 'blur'}]">
                         <el-input v-model="userForm.username" placeholder="请输入账号" style="width: 100%;"></el-input>
                     </el-form-item>
+                    <el-divider />
                     <el-form-item prop="password" label="密码" :rules="[{required: true,message: '请输入密码',trigger: 'blur'}]">
                         <el-input v-model="userForm.password" type="password" show-password placeholder="请输入密码" style="width: 100%;"></el-input>
                     </el-form-item>
+                    <el-divider />
                     <el-form-item prop="phone" label="手机号" :rules="[
                         {required: true, message: '请输入手机号', trigger: 'blur'},
                         {pattern: /^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/, message: '请输入正确的手机号', trigger: 'blur'}]">
                         <el-input v-model="userForm.phone" placeholder="请输入手机号"></el-input>
                     </el-form-item>
+                    <el-divider />
                     <el-form-item prop="idCard" label="身份证" :rules="[
                         {required: true, message: '请输入身份证号码', trigger: 'blur'},
-                        {pattern: /^[1-9]\d{5}(18|19|20)\d{2}((0[1-9])|(1[0-2]))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$/, message: '请输入正确的身份证号码', trigger: 'blur'}]">
-                        <el-input v-model="userForm.idCard" type="password" show-password placeholder="请输入身份证号码"></el-input>
+                        {pattern: /^[1-9]\d{5}(18|19|20)\d{2}((0[1-9])|(1[0-2]))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$/, message: '请输入正确的身份证号', trigger: 'blur'}]">
+                        <el-input v-model="userForm.idCard" type="password" show-password placeholder="请输入身份证号"></el-input>
                     </el-form-item>
+                    <el-divider />
                     <el-form-item prop="email" label="邮箱" :rules="[
                         {required: true, message: '请输入邮箱地址', trigger: 'blur'},
                         {pattern: /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\.[a-zA-Z0-9-]+)*\.(com|cn|net)$/, message: '请输入正确的邮箱地址', trigger: 'blur'}]">
                         <el-input v-model="userForm.email" placeholder="请输入邮箱地址"></el-input>
                     </el-form-item>
+                    <el-divider />
                     <div style="padding-bottom: 2rem">
                         <el-button @click="onLogin" type="primary" link>已有账号，去登录。</el-button>
                     </div>
                     <div style="padding-bottom: 1rem">
-                        <el-button @click="onSubmit" type="primary" size="large" round style="width: 100%;">提交</el-button>
+                        <el-button @click="onSubmit" :loading="loading" type="primary" size="large" round style="width: 100%;">提交</el-button>
                     </div>
                 </el-form>
             </el-card>
@@ -39,7 +42,7 @@
     </div>
 </template>
 <script setup>
-    import {reactive, ref, unref, toRefs} from 'vue'
+    import {onBeforeMount, reactive, ref, unref, toRefs} from 'vue'
     import {ElMessage} from 'element-plus'
     import {useRouter} from 'vue-router'
     import {register} from '@/api/admin/user'
@@ -48,6 +51,8 @@
     const userFormRef = ref(null)
     const state = reactive({
         shadow: 'always',
+        loading: false,
+        defaultStyle: null,
         userForm: {
             username: '',
             password: '',
@@ -59,8 +64,14 @@
     })
 
     const {
-        shadow,userForm
+        shadow,loading,defaultStyle,userForm
     } = toRefs(state)
+
+    onBeforeMount(() => {
+        state.defaultStyle = {
+            height: (document.body.clientHeight || document.documentElement.clientHeight) + "px",
+        };
+    })
 
     const onSubmit = async () =>{
         const form = unref(userFormRef)
@@ -68,9 +79,11 @@
         await form.validate()
 
         // 注册成功去登录
+        state.loading = true;
         register(state.userForm).then(res=>{
             if(res){
                 ElMessage.success("注册成功！");
+                state.loading = false;
                 onLogin();
             }
         })
@@ -83,17 +96,29 @@
 </script>
 <style lang="scss">
     .register-container{
-        height: 100%;
-        .register-header{
-            text-align: center;
-            color: #fff;
-            font-size: 20px;
-            padding: 4rem 0;
-            background: var(--theme);
-        }
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-image: linear-gradient(to bottom, #f3e7e9 0%, #e3eeff 99%, #e3eeff 100%);
         .register-main{
+            padding: 0 3rem;
+            .main-title{
+                font-size: 2rem;
+                padding-bottom: 2rem;
+                font-weight: 700;
+            }
+            .el-divider--horizontal{
+                margin: 0 0 1.6rem 0;
+                border-top: 2px solid #A8ABB2;
+            }
+            .el-form-item,.el-form-item--default{
+                margin-bottom: 0.4rem;
+            }
+            .el-form-item__error{
+                padding-top: 15px;
+            }
             .el-card{
-                margin: -2rem 2rem 0 2rem;
+                border-radius: 2rem;
             }
             .el-input__wrapper{
                 border: none !important;
